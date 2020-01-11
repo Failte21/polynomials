6f89b4abf2f4b00994c58c864b31b7bc06857039
jean <lsimon@greenflex.com>
2020-01-11 15:22:31 +0100

    seemingly working pMult

--- test/PolynomialSpec.hs
+++ test/PolynomialSpec.hs
@@ -9,3 +9,9 @@ spec = do
     $          it "Should be [1, 2, 1]"
     $          multP [1, 1] [1, 1]
     `shouldBe` [1, 2, 1]
+
+  describe "multP"
+    $          context "with [0, 0, 3] [2, 3]"
+    $          it "Should be [0, 0,, 6, 9]"
+    $          multP [0, 0, 3] [2, 3]
+    `shouldBe` [0, 0, 6, 9]
